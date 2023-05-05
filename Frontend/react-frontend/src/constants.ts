fc457e0d9e375f32b36740a77b9fe28edf5db4f0
denistvein1 <denis.tvein@gmail.com>
2023-05-05 18:16:21 +0300

    update

--- Frontend/react-frontend/src/constants.ts
+++ Frontend/react-frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_API_URL = "/api/";
+export const BACKEND_API_URL = "/api";
 // export const BACKEND_API_URL = "http://localhost/api";
 export const SHOW_NOTIFICATION = "SHOW_NOTIFICATION";
 export const ERROR_MESSAGE = "An error has occured!";
