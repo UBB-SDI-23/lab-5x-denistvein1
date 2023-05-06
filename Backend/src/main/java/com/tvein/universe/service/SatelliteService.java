7ef953410b0bda00cf22eb002f7441a9a95e256e
denistvein1 <denis.tvein@gmail.com>
2023-05-06 22:27:55 +0300

    update

--- Backend/src/main/java/com/tvein/universe/service/SatelliteService.java
+++ Backend/src/main/java/com/tvein/universe/service/SatelliteService.java
@@ -10,7 +10,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
