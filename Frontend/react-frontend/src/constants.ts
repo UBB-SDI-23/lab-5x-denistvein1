9494e927546eea92bc6e1c29e098f86301c9f0ee
denistvein1 <denis.tvein@gmail.com>
2023-05-07 14:14:19 +0300

    update

--- Frontend/react-frontend/src/constants.ts
+++ Frontend/react-frontend/src/constants.ts
@@ -1,5 +1,11 @@
-export const BACKEND_API_URL = "/api";
+// export const BACKEND_API_URL = "/api";
 // export const BACKEND_API_URL = "http://localhost/api";
+
+const PROD_API_URL = 'https://www.onlyhater.com/api';
+const DEV_API_URL = "http://localhost/api";
+
+export const BACKEND_API_URL = process.env.NODE_ENV === 'development' ? DEV_API_URL : PROD_API_URL;
+
 export const SHOW_NOTIFICATION = "SHOW_NOTIFICATION";
 export const ERROR_MESSAGE = "An error has occured!";
 export const SEVERITY_SUCCESS = "success";
